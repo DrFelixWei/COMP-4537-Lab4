154e616fef6b70d3bb3ed7d37c98ce2ecb7d30fd
Niko Durano <nikodurano@gmail.com>
2025-02-05 13:04:38 -0800

    store word function added

--- backend/app.js
+++ backend/app.js
@@ -95,3 +95,17 @@ function getWord(word){
   };
 
 }
+
+function storeWord(word, definition){
+  const xhttp = new XMLHttpRequest();
+  const url = `http://localhost:8080/api/definitions`;
+  xhttp.open('POST', url, true);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.send(JSON.stringify({ word, definition }));
+  xhttp.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      const response = JSON.parse(this.responseText);
+      console.log(response);
+    }
+  };
+}
